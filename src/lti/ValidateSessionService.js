b73915ff11a0eb3be9acdffd6fe69b7464055bad
James Stanley <jstanley@unicon.net>
2020-11-08 17:35:12 -0700

    set api name

--- src/lti/ValidateSessionService.js
+++ src/lti/ValidateSessionService.js
@@ -1,9 +1,9 @@
 import API from "@aws-amplify/api";
-import { LTI_API_NAME, LTI_SESSION_VALIDATION_ENDPOINT, logger } from "@asu-etx/rl-shared";
+import { LTI_SESSION_VALIDATION_ENDPOINT, logger } from "@asu-etx/rl-shared";
 
 API.configure();
 
-
+const LTI_API_NAME = "ringleaderapi";
 const hasValidSession = async () => {
   const hasValidSession = await API.get(
     LTI_API_NAME,
