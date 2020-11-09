eda640d6022e7f68490227d15e4a9029162cdbd9
James Stanley <jstanley@unicon.net>
2020-11-09 08:10:57 -0700

    update to use aws_exports

--- src/lti/ValidateSessionService.js
+++ src/lti/ValidateSessionService.js
@@ -1,6 +1,8 @@
 import API from "@aws-amplify/api";
 import { LTI_SESSION_VALIDATION_ENDPOINT, logger } from "@asu-etx/rl-shared";
+import aws_exports from '../aws-exports';
 
+API.configure(aws_exports);
 console.log(`api configuration: ${JSON.stringify(API.configure())}`);
 
 const LTI_API_NAME = "ringleaderapi";
