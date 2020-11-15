a9bc2b350899a64bc2960cee6a3d1d43e63c97da
Dune Thomas <dune.thomas@smartsparrow.com>
2020-11-15 15:38:16 -0800

    put fetchGradeForStudent back in place

--- src/utils/RingLeader.js
+++ src/utils/RingLeader.js
@@ -123,16 +123,16 @@ export function fetchUnassignedStudents(courseId, assignmentId) {
  *
  * NOTE: A grade only exists for homework that has been fully graded and sent to the LMS grade book.
  */
-// export function fetchGradeForStudent(assignmentId, studentId) {
-//   if (window.isDevMode) return mockGetStudentGrade(assignmentId, studentId);
-//
-//   // Temp solution until we have additional method.
-//   let allGrades = fetchAllGrades(assignmentId);
-//   return allGrades.find(g => g.studentId === studentId);
-//
-//   // TODO: We need a RL method that gets a single student id. (We don't want a student to be able to fetch ids of all students)
-//   // return realGetStudentGrade(assignmentId, studentId);
-// }
+export function fetchGradeForStudent(assignmentId, studentId) {
+  if (window.isDevMode) return mockGetStudentGrade(assignmentId, studentId);
+
+  // Temp solution until we have additional method.
+  let allGrades = fetchAllGrades(assignmentId);
+  return allGrades.find(g => g.studentId === studentId);
+
+  // TODO: We need a RL method that gets a single student id. (We don't want a student to be able to fetch ids of all students)
+  // return realGetStudentGrade(assignmentId, studentId);
+}
 
 
 /**
