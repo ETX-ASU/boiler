e29259de89388966d49abf0c1a4255283ce3d808
jstanley <jstanley70@gmail.com>
2020-11-25 09:03:02 -0700

    removed dotenv requirements

--- amplify/backend/function/ltilambda/src/index.js
+++ amplify/backend/function/ltilambda/src/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+//require('dotenv').config();
 const awsServerlessExpress = require('aws-serverless-express');
 
 //TOOL_CONSUMERS should be set up through the amplify console ui, environment variables.
+// local is left as an example of what you should be setting.
 //const environment = process.env.environment ? process.env.environment : "local";
 //process.env.TOOL_CONSUMERS = process.env.TOOL_CONSUMERS ? process.env.TOOL_CONSUMERS : JSON.stringify(require(`./environments/${environment}/.tool_consumers.${environment}.json`));
 const app = require('./app-cached');
