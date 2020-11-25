35fd869834023844dc93304b49ebed09b98cea7c
jstanley <jstanley70@gmail.com>
2020-11-25 15:05:20 -0700

    revert attempt to use backend variables.

--- amplify/backend/function/ltilambda/src/index.js
+++ amplify/backend/function/ltilambda/src/index.js
@@ -1,11 +1,11 @@
-//require('dotenv').config();
+require('dotenv').config();
 const { logger } = require('@asu-etx/rl-shared');
 const awsServerlessExpress = require('aws-serverless-express');
 
 //TOOL_CONSUMERS should be set up through the amplify console ui, environment variables.
 // local is left as an example of what you should be setting.
-//const environment = process.env.environment ? process.env.environment : "local";
-//process.env.TOOL_CONSUMERS = process.env.TOOL_CONSUMERS ? process.env.TOOL_CONSUMERS : JSON.stringify(require(`./environments/${environment}/.tool_consumers.${environment}.json`));
+const environment = process.env.environment ? process.env.environment : "local";
+process.env.TOOL_CONSUMERS = process.env.TOOL_CONSUMERS ? process.env.TOOL_CONSUMERS : JSON.stringify(require(`./environments/${environment}/.tool_consumers.${environment}.json`));
 
 
 const app = require('./app-cached');
