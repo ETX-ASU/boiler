6d782dcb04a8eba214bf9b7984c3488eb2f70eb0
jstanley <jstanley70@gmail.com>
2020-11-25 10:03:18 -0700

    adding logging

--- amplify/backend/function/ltilambda/src/index.js
+++ amplify/backend/function/ltilambda/src/index.js
@@ -1,15 +1,19 @@
 //require('dotenv').config();
+const { logger } = require('@asu-etx/rl-shared');
 const awsServerlessExpress = require('aws-serverless-express');
 
 //TOOL_CONSUMERS should be set up through the amplify console ui, environment variables.
 // local is left as an example of what you should be setting.
 //const environment = process.env.environment ? process.env.environment : "local";
 //process.env.TOOL_CONSUMERS = process.env.TOOL_CONSUMERS ? process.env.TOOL_CONSUMERS : JSON.stringify(require(`./environments/${environment}/.tool_consumers.${environment}.json`));
+
+
 const app = require('./app-cached');
 
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
+  logger.debug("process.env:" + JSON.stringify(process.env));
   console.log(`EVENT: ${JSON.stringify(event)}`);
   return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
