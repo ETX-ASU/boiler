021a861e4850f1f67067b9b4dd699e4a698f6498
James Stanley <jstanley@unicon.net>
2021-06-16 14:08:35 -0700

    fixed typo

--- amplify/backend/function/ltilambda/src/index.js
+++ amplify/backend/function/ltilambda/src/index.js
@@ -5,7 +5,7 @@ const awsServerlessExpress = require('aws-serverless-express');
 //TOOL_CONSUMERS should be set up through the amplify console ui, environment variables.
 // local is left as an example of what you should be setting.do
 const environment = process.env.environment ? process.env.environment : "stage";
-process.env.TOOL_CONSUMERS = process.env.TOOL_CONSUMERS ? process.env.TOOL_CONSUMERS : JSON.stringify(require(`./environments/${environment}/.tool_consumers.${environment}.json`));
+process.env.TOOL_CONSUMERS = process.env.TOOL_CONSUMERS ? process.env.TOOL_CONSUMERS : JSON.stringify(require(`./environments/${environment}/tool_consumers.${environment}.json`));
 
 
 const app = require('./app-cached');
