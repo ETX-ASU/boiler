fbec0deb0b0df6b78275716102a31cf83f2b015e
James Stanley <jstanley@unicon.net>
2021-12-07 11:08:13 -0700

    updates to test canvas endpoints

--- amplify/backend/function/ltilambda/src/index.js
+++ amplify/backend/function/ltilambda/src/index.js
@@ -1,10 +1,11 @@
-require('dotenv').config({path:`${__dirname}/env`})
+require('dotenv').config({path:`${__dirname}/env`});
+const environment = process.env.environment ? process.env.environment : "stage";
+require('dotenv').config({path:`${__dirname}/environments/${environment}/env.${environment}`});
 const { logger } = require('@asu-etx/rl-shared');
 const awsServerlessExpress = require('aws-serverless-express');
 
 //TOOL_CONSUMERS should be set up through the amplify console ui, environment variables.
 // local is left as an example of what you should be setting.do
-const environment = process.env.environment ? process.env.environment : "dev";
 process.env.TOOL_CONSUMERS = process.env.TOOL_CONSUMERS ? process.env.TOOL_CONSUMERS : JSON.stringify(require(`./environments/${environment}/tool_consumers.${environment}.json`));
 
 
--- src/lti-exports.js
+++ src/lti-exports.js
@@ -6,7 +6,7 @@ const awsmobile = {
     "aws_cloud_logic_custom": [
         {
             "name": "ringleaderapi",
-            "endpoint": "https://qx0moni8xe.execute-api.us-west-2.amazonaws.com/prod",
+            "endpoint": "https://yqgnh270oj.execute-api.us-west-2.amazonaws.com/prod",
             "region": "us-west-2"
         }
     ]
